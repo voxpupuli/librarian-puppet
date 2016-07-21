ed3cd17d2ae6e52bab2c4da3be1cfbdf6d63bfc7
Brian Hawkins <brianhks1@gmail.com>
2016-07-21 17:17:33 -0600

    Removed dependency on puppet command line

--- spec/source/forge_spec.rb
+++ spec/source/forge_spec.rb
@@ -21,33 +21,4 @@ describe Forge do
     end
   end
 
-  describe "#check_puppet_module_options" do
-    before do
-      expect(Librarian::Puppet).to receive(:puppet_version).at_least(:once) { puppet_version }
-      expect(Librarian::Puppet).to receive(:puppet_gem_version).at_least(:once) { Gem::Version.create(puppet_version.split(' ').first.strip.gsub('-', '.')) }
-    end
-    context "Puppet OS" do
-      context "3.4.3" do
-        let(:puppet_version) { "3.4.3" }
-        it { Forge.client_api_version().should == 1 }
-      end
-      context "2.7.13" do
-        let(:puppet_version) { "2.7.13" }
-        it { Forge.client_api_version().should == 1 }
-      end
-      context "3.6.0" do
-        it { Forge.client_api_version().should == 3 }
-      end
-    end
-    context "Puppet Enterprise" do
-      context "3.2.1" do
-        let(:puppet_version) { "3.4.3 (Puppet Enterprise 3.2.1)" }
-        it { Forge.client_api_version().should == 3 }
-      end
-      context "3.1.3" do
-        let(:puppet_version) { "3.3.3 (Puppet Enterprise 3.1.3)" }
-        it { Forge.client_api_version().should == 1 }
-      end
-    end
-  end
 end
